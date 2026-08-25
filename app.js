039cbe717a1cbcf32a2d4fdb5139db093e60ef0e
lucas <leitecostalucas2@gmail.com>
2026-08-25 16:16:31 -0300

    Mudança de 10 para 100 de limite

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroAleatorio = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -12,7 +12,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function mensagemInicial(){
 exibirTextoNaTela('h1', 'Jogo do Número Aleatório');
-exibirTextoNaTela('p', 'Escolha um número entre 1 e 10!');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 100!');
 }
 
 mensagemInicial();
